18ea12e763af1c4c6ad7ca5fa9f20be3e070ada0
Wade Foster <jwade109@vt.edu>
2019-05-06 18:06:23 -0400

    Make idiomatic, expressive
    mmm

--- relay/relay.ino
+++ relay/relay.ino
@@ -1,257 +1,204 @@
 #include <CircularBuffer.h>
 #include <SPI.h>
 #include <RH_RF95.h>
-#define RFM95_CS 4
-#define RFM95_RST 2
-#define RFM95_INT 3
-#define RF95_FREQ 434.0
+
+const uint8_t RFM95_CS = 4;
+const uint8_t RFM95_RST = 2;
+const uint8_t RFM95_INT = 3;
+const double RF95_FREQ = 434.0;
+const size_t BUFFER_SIZE = RH_RF95_MAX_MESSAGE_LEN * 2;
+const uint8_t REPLY_PACKET[6] = {0xAA, 0x14, 0x69, 0x00, 0x13, 0x14};
+const uint32_t serial_baudrate = 38400;
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-const size_t BUFFER_SIZE = 256;
 CircularBuffer<uint8_t, BUFFER_SIZE> buf1, buf2, buf3, buf4;
 
-CircularBuffer<uint8_t, BUFFER_SIZE> parse_r(CircularBuffer<uint8_t, BUFFER_SIZE>* buf);
-void send_r(CircularBuffer<uint8_t, BUFFER_SIZE>* buffer, HardwareSerial stream1, HardwareSerial stream2, HardwareSerial stream3, uint8_t len, bool);
-void fill_r(CircularBuffer<uint8_t, BUFFER_SIZE>* buffer, HardwareSerial *dstream);
-void fill_spi(CircularBuffer<uint8_t, BUFFER_SIZE>* buffer);
+/**
+ * Parses the provided buffer for packets, and removes all bytes which were processed.
+ */
+CircularBuffer<uint8_t, BUFFER_SIZE> parse_packet(CircularBuffer<uint8_t, BUFFER_SIZE>& buf);
 
+/**
+ * Gets all data from a serial port, up to the maximum size of the circular buffer.
+ */
+void serial_receive(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer, HardwareSerial &dstream);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial1.begin(38400);
-  Serial2.begin(38400);
-  Serial3.begin(38400);
-  //Serial.begin(38400);
-  pinMode(RFM95_RST, OUTPUT);
-  digitalWrite(RFM95_RST, HIGH);
+/**
+ * Listens for a short time and stores all data from the radio,
+ * up to the maximum size of the buffer.
+ */
+void radio_recieve(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer);
 
-  Serial.begin(38400);
-  delay(100);
+/**
+ * Writes a data buffer to 2 serial ports and the radio, and sends a confirmation
+ * packet to the source serial port.
+ */
+void echo_serial(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer,
+    HardwareSerial &source, HardwareSerial &dest1, HardwareSerial &dest2);
 
-  digitalWrite(RFM95_RST, LOW);
-  delay(10);
-  digitalWrite(RFM95_RST, HIGH);
-  delay(10);
+/**
+ * Writes a data buffer to 3 serial ports, and sends a confirmation packet
+ * over the radio.
+ */
+void echo_radio(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer,
+    HardwareSerial &dest1, HardwareSerial &dest2, HardwareSerial &dest3);
 
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial1.begin(serial_baudrate);
+    Serial2.begin(serial_baudrate);
+    Serial3.begin(serial_baudrate);
+    Serial.begin(serial_baudrate);
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: ");
-  Serial.println(RF95_FREQ);
-  rf95.setTxPower(23, false);
+    pinMode(RFM95_RST, OUTPUT);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(100);
+    digitalWrite(RFM95_RST, LOW);
+    delay(10);
+    digitalWrite(RFM95_RST, HIGH);
+    delay(10);
+
+    while (!rf95.init())
+    {
+        Serial.println("LoRa radio init failed");
+        while (1);
+    }
+    Serial.println("LoRa radio init OK!");
+
+    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+    if (!rf95.setFrequency(RF95_FREQ))
+    {
+        Serial.println("setFrequency failed");
+        while (1);
+    }
+
+    Serial.print("Set Freq to: ");
+    Serial.println(RF95_FREQ);
+    rf95.setTxPower(23, false);
 
 }
 
 void loop() {
-  //  rf95.send((uint8_t*)(0x69), sizeof(0x69));
-  //      delay(10);
-  //      rf95.waitPacketSent();
-  Serial.println("hello");
 
-  fill_r(&buf1, &Serial1);
-  fill_r(&buf2, &Serial2);
-  fill_r(&buf3, &Serial3);
-  fill_spi(&buf4);
-  //fill_r(&buf4, &Serial);
+    serial_receive(buf1, Serial1);
+    serial_receive(buf2, Serial2);
+    serial_receive(buf3, Serial3);
+    radio_recieve(buf4);
 
-  Serial.print("buf1 size: ");
-  Serial.println(buf1.size());
-  Serial.print("buf2 size: ");
-  Serial.println(buf2.size());
-  Serial.print("buf3 size: ");
-  Serial.println(buf3.size());
-  Serial.print("buf4 size: ");
-  Serial.println(buf4.size());
+    CircularBuffer<uint8_t, BUFFER_SIZE> from1 = parse_packet(buf1);
+    CircularBuffer<uint8_t, BUFFER_SIZE> from2 = parse_packet(buf2);
+    CircularBuffer<uint8_t, BUFFER_SIZE> from3 = parse_packet(buf3);
+    CircularBuffer<uint8_t, BUFFER_SIZE> fromRadio = parse_packet(buf4);
 
-  Serial.println("before first parse");
-  CircularBuffer<uint8_t, BUFFER_SIZE> packet1 = parse_r(&buf1);
-  Serial.print("packet1 size: ");
-  Serial.println(packet1.size());
-  Serial.println("after first parse");
-  CircularBuffer<uint8_t, BUFFER_SIZE> packet2 = parse_r(&buf2);
-  Serial.println("after second parse");
-  CircularBuffer<uint8_t, BUFFER_SIZE> packet3 = parse_r(&buf3);
-  Serial.println("after third parse");
-  CircularBuffer<uint8_t, BUFFER_SIZE> packet4 = parse_r(&buf4);
-  Serial.println("after fourth parse");
-  Serial.print("packet4 size: ");
-  Serial.println(packet4.size());
-
-  send_r(&packet1, Serial2, Serial3, Serial1, packet1.size(), true);
-  Serial.println("after first send");
-  send_r(&packet2, Serial1, Serial3, Serial2, packet2.size(), true);
-  Serial.println("after second send");
-  send_r(&packet3, Serial1, Serial2, Serial3, packet3.size(), true);
-  Serial.println("after third send");
-  send_r(&packet4, Serial1, Serial2, Serial3, packet4.size(), false);//should be false
-  Serial.println("after fourth send");
-
-  delay(1000);
+    // echoes, and sends confirmation to, the first serial port
+    echo_serial(from1, Serial1, Serial2, Serial3);
+    echo_serial(from2, Serial2, Serial1, Serial3);
+    echo_serial(from3, Serial3, Serial1, Serial2);
+    // echoes, and sends confirmation to, the radio
+    echo_radio(fromRadio, Serial1, Serial2, Serial3);
 }
 
-void fill_r(CircularBuffer<uint8_t, BUFFER_SIZE>* buffer, HardwareSerial *dstream)
+void serial_receive(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer, HardwareSerial &dstream)
 {
-  while (buffer->available() > 0 && dstream->available() > 0)
-  {
-    buffer->push(dstream->read());
-  }
+    while (buffer.available() > 0 && dstream.available() > 0)
+    {
+        buffer.push(dstream.read());
+    }
 }
 
-void fill_spi(CircularBuffer<uint8_t, BUFFER_SIZE>* buffer)
+void radio_recieve(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer)
 {
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(100))
-  {
-    if (rf95.recv(buf, &len))
+    if (rf95.waitAvailableTimeout(100))
     {
-      Serial.println("Got Reply: ");
-      for (int i = 0; i < len; i++)
-      {
-        buffer->push(*(buf + i));
-        Serial.print("Data: ");
-        Serial.println(*(buf + i));
-        Serial.print("buffer size in fill: ");
-        Serial.println(buffer->size());
-      }
+        if (rf95.recv(buf, &len))
+        {
+            Serial.println("Got Reply: ");
+            for (int i = 0; i < len && buffer.available() > 0; i++)
+            {
+                buffer.push(buf[i]);
+            }
+        }
     }
-    else
-    {
-      Serial.println("Receive failed you goddamn scrub");
-    }
-  }
 }
 
-CircularBuffer<uint8_t, BUFFER_SIZE> parse_r(CircularBuffer<uint8_t, BUFFER_SIZE>* buf)
+CircularBuffer<uint8_t, BUFFER_SIZE> parse_packet(CircularBuffer<uint8_t, BUFFER_SIZE> &buf)
 {
-  Serial.println("in parse");
-  bool parsing = true;
-  CircularBuffer<uint8_t, BUFFER_SIZE> packet;
-  while (parsing && buf->size() > 0)
-  {
-    Serial.println("in firstwhile");
-    while (buf->size() > 0 && buf->first() != 0xAA)
+    bool parsing = true;
+    CircularBuffer<uint8_t, BUFFER_SIZE> packet;
+    while (parsing && buf.size() > 0)
     {
-      Serial.println("looking for first 0xAA");
-      buf->shift();
-    }
+        Serial.println("in firstwhile");
+        while (buf.size() > 0 && buf.first() != 0xAA)
+        {
+            buf.shift();
+        }
 
-    if (buf->size() < 2)
-    {
-      Serial.println("doesnt have at least 2 bytes yet");
-      continue;
-    }
+        if (buf.size() < 2)
+        {
+            continue;
+        }
 
-    if ((*buf)[1] != 0x14)
-    {
-      Serial.println("2nd byte not right");
-      buf->shift();
-      continue;
-    }
+        if (buf[1] != 0x14)
+        {
+            buf.shift();
+            continue;
+        }
 
-    if (buf->size() < 4)
-    {
-      Serial.println("havent recieved full header yet");
-      parsing = false;
-      continue;
-    }
+        if (buf.size() < 4)
+        {
+            parsing = false;
+            continue;
+        }
 
-    uint8_t length = (*buf)[2];
-    Serial.print("length: ");
-    Serial.println(length);
+        uint8_t length = buf[2];
+        if (buf.size() < 6 + length)
+        {
+            parsing = false;
+            continue;
+        }
 
-    if (buf->size() < 6 + length)
-    {
-      Serial.println("packet header recieved, waiting for data");
-      parsing = false;
-      continue;
+        for (int i = 0; i < 6 + length; ++i)
+        {
+            packet.push(buf.shift());
+        }
+        return packet;
     }
-
-    for (int i = 0; i < 6 + length; ++i)
-    {
-      packet.push(buf->shift());
-    }
-    Serial.println("Packet fully received");
     return packet;
-  }
-  Serial.println("no packet");
-  return packet;
 }
 
-void send_r(CircularBuffer<uint8_t, BUFFER_SIZE> *buffer, HardwareSerial stream1, HardwareSerial stream2, HardwareSerial stream3, uint8_t len, bool spi)
+void echo_serial(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer,
+                 HardwareSerial &source, HardwareSerial &dest1, HardwareSerial &dest2)
 {
-  Serial.println("before while in send");
-  uint8_t toSend[len];
-  uint8_t received[1] = {0x69};
-  size_t index = 0;
-  while (buffer->size() > 0)
-  {
-    Serial.println("in while loop");
-    if (spi) //should just return received key to spi. This nonsense should be in else
+    uint8_t toSend[buffer.size()];
+    const size_t bufferSize = buffer.size();
+    for (size_t i = 0; buffer.size() > 0; i++)
     {
-      toSend[index] = buffer->first();
-      index++;
-      if (stream1)
-      {
-        Serial.println("Trying to send to stream 1");
-        stream1.write(buffer->first());
-        Serial.println("Sent to stream 1");
-      }
-      if (stream2)
-      {
-        Serial.println("Trying to send to stream 2");
-        stream2.write(buffer->shift());
-        Serial.println("Sent to stream 2");
-      }
-      if (stream3)
-      {
-        stream3.write(0x69);//packet received message
-      }
+        toSend[i] = buffer.shift();
+    }
 
-    }
-    else
-    {
-      if (stream1) {
-        Serial.println("Trying to send to stream 1");
-        stream1.write(buffer->first());
-        Serial.println("sent to stream 1");
-      }
-      if (stream2) {
-        Serial.println("Trying to send to stream 2");
-        stream2.write(buffer->first());
-        Serial.println("sent to stream 2");
-      }
-      if (stream3) {
-        Serial.println("Trying to send to stream 3");
-        stream3.write(buffer->shift());
-        Serial.println("sent to stream 3");
-      }
-    }
-  }
-  if (spi)
-  {
-    if (index > 0)
-    {
-      rf95.send(toSend, sizeof(toSend));
-    }
-  }
-  else
-  {
-    if (len > 0)
-    {
-      rf95.send(received, sizeof(received));
-    }
-  }
+    source.write(REPLY_PACKET, sizeof(REPLY_PACKET));
+    dest1.write(toSend, bufferSize);
+    dest2.write(toSend, bufferSize);
+    rf95.send(toSend, bufferSize);
+}
 
+void echo_radio(CircularBuffer<uint8_t, BUFFER_SIZE> &buffer,
+                HardwareSerial &dest1, HardwareSerial &dest2, HardwareSerial &dest3)
+{
+    uint8_t toSend[buffer.size()];
+    const size_t bufferSize = buffer.size();
+    for (size_t i = 0; buffer.size() > 0; i++)
+    {
+        toSend[i] = buffer.shift();
+    }
+
+    rf95.send(REPLY_PACKET, sizeof(REPLY_PACKET));
+    dest1.write(toSend, bufferSize);
+    dest2.write(toSend, bufferSize);
+    dest3.write(toSend, bufferSize);
 }
