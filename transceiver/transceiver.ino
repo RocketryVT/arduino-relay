a2ed39d55f10001c346bd65b8fd71dda478eaa69
Wade Foster <jwade109@vt.edu>
2019-05-09 14:07:14 -0400

    Remove all CRLF, make simpler

--- transceiver/transceiver.ino
+++ transceiver/transceiver.ino
@@ -9,29 +9,29 @@ const double RF95_FREQ = 434.0;
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-void setup() 
+void setup()
 {
     pinMode(RFM95_RST, OUTPUT);
     digitalWrite(RFM95_RST, HIGH);
-  
+
     Serial.begin(38400);
     delay(100);
-    
+
     digitalWrite(RFM95_RST, LOW);
     delay(10);
     digitalWrite(RFM95_RST, HIGH);
     delay(10);
-  
+
     while (!rf95.init()) {
-      Serial.println("LoRa radio init failed");
-      while (1);
+        Serial.println("LoRa radio init failed");
+        while (1);
     }
     Serial.println("LoRa radio init OK!");
-  
+
     // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
     if (!rf95.setFrequency(RF95_FREQ)) {
-      Serial.println("setFrequency failed");
-      while (1);
+        Serial.println("setFrequency failed");
+        while (1);
     }
     Serial.print("Set Freq to: ");
     Serial.println(RF95_FREQ);
@@ -40,29 +40,20 @@ void setup()
 
 void loop()
 {
-    uint8_t toSend[Serial.available()];
+    size_t available = Serial.available();
+    uint8_t toSend[available];
+    Serial.readBytes(toSend, available);
 
-    size_t index = 0;
-    while (Serial.available() > 0)
+    if (available > 0)
     {
-        toSend[index] = Serial.read();
-        index++;
+        rf95.send(toSend, available);
     }
 
-    if (index > 0)
-    {
-        rf95.send(toSend, sizeof(toSend));
-    }
-  
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
 
-    if (rf95.waitAvailableTimeout(100))
-    { 
-        if (rf95.recv(buf, &len))
-        {
-            for (int i = 0; i < len; i++) Serial.print((char) buf[i]);
-            Serial.println();
-        }
-   }
+    if (rf95.waitAvailableTimeout(100) && rf95.recv(buf, &len))
+    {
+        Serial.write(buf, len);
+    }
 }
