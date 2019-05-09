8fc28237547ddbc1fe61e6553bde9934951367aa
Wade Foster <jwade109@vt.edu>
2019-05-09 17:47:32 -0400

    Add simple echo program

---
+++ echo/echo.ino
@@ -0,0 +1,11 @@
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    uint8_t buffer[Serial.available()];
+    Serial.readBytes(buffer, sizeof(buffer));
+    Serial.write(buffer, sizeof(buffer));
+}
